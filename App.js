360b9b0a5fb60f64c68479ef8e4fc76a03198332
Sajith Wijerathne <sajith@vizuamatix.com>
2019-08-25 02:11:50 +0530

    added ToDoList screen

--- App.js
+++ App.js
@@ -3,12 +3,14 @@ import Home from './app/views/Home';
 import Notifications from './app/views/NotificationScreen';
 import ImageGrid from './app/views/ImageGrid';
 import ImageGrid2 from './app/views/ImageGrid2';
+import ToDoList from './app/views/ToDoList';
 
 const MainNavigator = createStackNavigator({
   Home: {screen: Home},
   Notifications: {screen: Notifications},
   ImageGrid: {screen: ImageGrid},
-  ImageGrid2: {screen: ImageGrid2}
+  ImageGrid2: {screen: ImageGrid2},
+  ToDoList:{screen: ToDoList}
 });
 
 const App = createAppContainer(MainNavigator);
