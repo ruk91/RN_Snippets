53a5df0d5de723328f98c04865aa281676523d8c
Sajith Wijerathne <sajith@vizuamatix.com>
2019-08-25 02:08:17 +0530

    added ImageGrid2 screen

--- App.js
+++ App.js
@@ -2,11 +2,13 @@ import {createStackNavigator, createAppContainer} from 'react-navigation';
 import Home from './app/views/Home';
 import Notifications from './app/views/NotificationScreen';
 import ImageGrid from './app/views/ImageGrid';
+import ImageGrid2 from './app/views/ImageGrid2';
 
 const MainNavigator = createStackNavigator({
   Home: {screen: Home},
   Notifications: {screen: Notifications},
-  ImageGrid: {screen: ImageGrid}
+  ImageGrid: {screen: ImageGrid},
+  ImageGrid2: {screen: ImageGrid2}
 });
 
 const App = createAppContainer(MainNavigator);
