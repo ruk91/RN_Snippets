142b4a9d0fe1a0a4e4c4b7e2622144aff1207c4c
Sajith Wijerathne <sajith@vizuamatix.com>
2019-08-25 02:05:18 +0530

    added Image grid screen

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import {createStackNavigator, createAppContainer} from 'react-navigation';
 import Home from './app/views/Home';
 import Notifications from './app/views/NotificationScreen';
+import ImageGrid from './app/views/ImageGrid';
 
 const MainNavigator = createStackNavigator({
   Home: {screen: Home},
   Notifications: {screen: Notifications},
+  ImageGrid: {screen: ImageGrid}
 });
 
 const App = createAppContainer(MainNavigator);
--- app/views/Home.js
+++ app/views/Home.js
@@ -20,7 +20,7 @@ export default class Craigslist extends Component {
       userSelected:[],
       data: [
         {id:1,  name: "Notifications",   image:"https://img.icons8.com/clouds/100/000000/groups.png",           count:124.711},
-        {id:2,  name: "Housing",    image:"https://img.icons8.com/color/100/000000/real-estate.png",       count:234.722},
+        {id:2,  name: "ImageGrid",    image:"https://img.icons8.com/color/100/000000/real-estate.png",       count:234.722},
         {id:3,  name: "Jobs",       image:"https://img.icons8.com/color/100/000000/find-matching-job.png", count:324.723} ,
         {id:4,  name: "Personal",   image:"https://img.icons8.com/clouds/100/000000/employee-card.png",    count:154.573} ,
         {id:5,  name: "For sale",   image:"https://img.icons8.com/color/100/000000/land-sales.png",        count:124.678} ,
