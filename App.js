4ea61898fd16c961c97e27f07f2fe829ba00087e
Sajith Wijerathne <sajith@vizuamatix.com>
2019-08-25 02:02:15 +0530

    Added home page

--- App.js
+++ App.js
@@ -1,7 +1,9 @@
 import {createStackNavigator, createAppContainer} from 'react-navigation';
+import Home from './app/views/Home';
 import Notifications from './app/views/NotificationScreen';
+
 const MainNavigator = createStackNavigator({
-  // Home: {screen: HomeScreen},
+  Home: {screen: Home},
   Notifications: {screen: Notifications},
 });
 
