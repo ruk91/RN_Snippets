b74b1b8c082411e9776d5681a684326039613150
Sajith Wijerathne <sajith@vizuamatix.com>
2019-08-25 02:22:16 +0530

    added WhatsAppCall List  screen

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ import ImageGrid2 from './app/views/ImageGrid2';
 import ToDoList from './app/views/ToDoList';
 import ModalDetail from './app/views/ModalDetail';
 import WhatsappCall from './app/views/WhatsappCall';
+import WhatsappCallList from './app/views/WhatsappCallList';
 
 const MainNavigator = createStackNavigator({
   Home: {screen: Home},
@@ -14,7 +15,8 @@ const MainNavigator = createStackNavigator({
   ImageGrid2: {screen: ImageGrid2},
   ToDoList: {screen: ToDoList},
   ModalDetail: {screen: ModalDetail},
-  WhatsappCall: {screen: WhatsappCall}
+  WhatsappCall: {screen: WhatsappCall},
+  WhatsappCallList: {screen: WhatsappCallList}
 });
 
 const App = createAppContainer(MainNavigator);
